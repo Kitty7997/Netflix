6a7e62d383900ef6105fba4d19190ae4f0ad68bd
Kitty Verma <vermatejender97@gmail.com>
2023-04-11 13:36:09 +0530

    change in index file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dot = require('dotenv')
-dot.config({ path: './backend/.env' }).parsed
+dot.config({ path: './backend/.env' })
 const app = express();
 const cors = require('cors')
 const db = require('./src/db/db')
@@ -21,5 +21,5 @@ app.use('', routers);
 // app.use(passport.initialize());
 // app.use(passport.session());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Kitty! I am live now'))
 app.listen(port, () => console.log(`App listening on port ${port}!`))
