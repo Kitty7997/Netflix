8e48ab1f13d3ceb0774973092656e76de2ddef4b
Kitty Verma <vermatejender97@gmail.com>
2023-04-12 11:54:55 +0530

    change in database file

--- src/db/db.js
+++ src/db/db.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+// Check if MONGODB_URI environment variable is defined and has a valid value
+if (!process.env.MONGODB_URI) {
+  console.error('MONGODB_URI is not defined in the environment variables.');
+  process.exit(1); // or throw an error, or handle the issue in your desired way
+}
+
+// Connect to MongoDB using the retrieved URI
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 mongoose.connection.once('open', function () {
-  console.log('Conection has been made!');
+  console.log('Database connected successfully!');
 }).on('error', function (error) {
   console.log('Error is: ', error);
-})
+});
